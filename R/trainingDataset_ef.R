282f30f567c94ec0ddd63f98b5a2b2686e85fda1
Ida <idajandl@gmx.de>
2020-08-05 07:51:00 +0200

    training Dataset_ef Umwandlung in imageset_ef

--- R/trainingDataset_ef.R
+++ R/trainingDataset_ef.R
@@ -2,8 +2,8 @@ library(tidyverse)
 source("R/helperFunctions_ef.R")
 source("R/image_ef.R")
 
-#Lädt .csv-Datei und speichert es als trainingDataset_ef
-load_trainingDataset_ef <- function(path, imgDim) {
+#Lädt .csv-Datei und speichert es als imageset_ef_ef
+load_imageset_ef <- function(path, imgDim) {
   #TODO: Baue Funktion ein, um Farbbilder zu laden
 
   #Lese Daten ein und speichere sie als [n_img, imgDim]-Array
@@ -12,33 +12,33 @@ load_trainingDataset_ef <- function(path, imgDim) {
   data %>% as.matrix() %>% as.double() %>% as.matrix() -> data
   dim(data) <- c(nrow, imgDim)
 
-  #Erstelle Objekt der Klasse trainingDataset_ef
+  #Erstelle Objekt der Klasse imageset_ef
   td <- list()
-  class(td) <- "trainingDataset_ef"
+  class(td) <- "imageset_ef"
 
-  #Füge die image_ef Objekte in das trainingDataset_ef Objekt ein
+  #Füge die image_ef Objekte in das imageset_ef Objekt ein
   for (i in 1:dim(data)[1]) td[[i]] <- image_ef(data[i,,])
 
   td
 }
 
-#Erstellt neues trainingDataset_ef mit den Werten aus lst
-trainingDataset_ef <- function(lst) {
+#Erstellt neues imageset_ef_ef mit den Werten aus lst
+imageset_ef <- function(lst) {
   stopifnot("Eingabe muss eine Liste sein" = is.list(lst))
   #TODO: teste, ob alle image_ef Objekte die gleiche imgDim haben
   #TODO: Gibt allen Einträgen der Liste die gleiche Dimension imgDim <- neuer Parameter der Funktion
 
-  class(lst) <- "trainingDataset_ef"
+  class(lst) <- "imageset_ef"
 
   lst
 }
 
-#Testet, ob eine Eingabe von der Klasse trainingDataset_ef ist
-is.trainingDataset_ef <- function(td) is.element("trainingDataset_ef", class(td))
+#Testet, ob eine Eingabe von der Klasse imageset_ef_ef ist
+is.imageset_ef <- function(td) is.element("imageset_ef", class(td))
 
 #Normalisiert die Gesichter, d.h. es zieht von jedem Pixel den über das Bild gemittelten Pixelwert ab
 normalize_faces <- function(td) {
-  stopifnot("Eingabe muss ein trainingDataset_ef sein" = is.trainingDataset_ef(td))
+  stopifnot("Eingabe muss ein imageset_ef sein" = is.imageset_ef(td))
   stopifnot("Eingabe muss mindestens die Länge 1 haben" = length(td)>1)
 
   for (i in 1:length(td)) {
@@ -49,7 +49,7 @@ normalize_faces <- function(td) {
 
 #Berechnet das Durchschnittsgesicht
 avg_face <- function(td) {
-  stopifnot("Eingabe muss ein trainingDataset_ef sein" = is.trainingDataset_ef(td))
+  stopifnot("Eingabe muss ein imageset_ef sein" = is.imageset_ef(td))
   stopifnot("Eingabe muss mindestens die Länge 1 haben" = length(td)>1)
 
   avg <- 0
@@ -63,7 +63,7 @@ avg_face <- function(td) {
 
 #Zieht das Durchschnittsgesicht von jedem Gesicht ab
 subtract_avg_face <- function(td) {
-  stopifnot("Eingabe muss ein trainingDataset_ef sein" = is.trainingDataset_ef(td))
+  stopifnot("Eingabe muss ein imageset_ef sein" = is.imageset_ef(td))
   stopifnot("Eingabe muss mindestens die Länge 1 haben" = length(td)>1)
 
   #Berechne Durchschnittsgesicht
@@ -78,7 +78,7 @@ subtract_avg_face <- function(td) {
 
 #Berechnet die Eigenwerte und Vektoren zur Kovarianzmatrix
 getEigenfaces <- function(td, nfaces = 15) {
-  stopifnot("Eingabe muss ein trainingDataset_ef sein" = is.trainingDataset_ef(td))
+  stopifnot("Eingabe muss ein imageset_ef sein" = is.imageset_ef(td))
   stopifnot("Eingabe muss mindestens die Länge 1 haben" = length(td)>1)
 
   #Normalisiere die Eigengesichter und ziehe das Durchschnittsgesicht ab
@@ -101,7 +101,7 @@ getEigenfaces <- function(td, nfaces = 15) {
   eigenvects <- eig$vectors[,1:cp]
   eigenvects <- A %*% eigenvects
 
-  #Erstelle aus den Eigenvektoren ein trainingDataset_ef
+  #Erstelle aus den Eigenvektoren ein imageset_ef
   imgDim <- dim(td[[1]])
 
   eigenfaces <- list()
@@ -112,7 +112,7 @@ getEigenfaces <- function(td, nfaces = 15) {
     eigenfaces[[i]] <- image_ef(eigenface)
   }
 
-  eigenfaces <- trainingDataset_ef(eigenfaces)
+  eigenfaces <- imageset_ef(eigenfaces)
 
   eigenfaces
 }

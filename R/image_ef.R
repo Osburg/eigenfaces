d80c39793d213d3843653b301c8aa644fa4a32af
Ida <idajandl@gmx.de>
2020-08-10 14:13:32 +0200

    Documentation of image_ef

--- R/image_ef.R
+++ R/image_ef.R
@@ -3,13 +3,15 @@ source("R/helperFunctions_ef.R")
 
 #' Object image_ef
 #'
-#' Creates objects of type image_ef.
-#' is.image_ef is more a general test of an object being interpretable as an image
-#' It is printed as an image in grayscale.
-#' @param x
-#' @return Returns image in grayscale.
-#'
+#' Creates objects of class image_ef.
+#' They are printed as images in grayscale.
+#' is.image_ef is more a general test of an object being interpretable as an image.
+#' @param x List of arrays.
+#' @return Returns image in grayscale.  ##Stimmt das?
+#' @usage image_ef(x)
+#' is.image_ef(x)
 #' @examples
+#' ##Todo: bessere Beispiele
 #' image_ef(x)
 #' is.image_ef(x)
 #' @export
@@ -28,6 +30,18 @@ print.image_ef <- function(img) imgShow_ef(img)
 
 #Prüfe, ob es sich um ein image_ef handelt
 is.image_ef <- function(img) is.element("image_ef", class(img))
+
+#' Normalize of image_ef objects
+#'
+#' Normalize an image_ef object.
+#'
+#' The image is subtracted by the sum of all images divided by their length.
+#' @param img an object of class 'image_ef'.
+#' @return Returns normalized version of img.
+#' @examples
+#' ##Todo: bessere Beispiele
+#' normalize(img)
+#' @export
 
 #Normalisiere image_ef Objekt
 normalize.image_ef <- function(img) {
